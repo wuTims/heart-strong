fcde67f2eb19a9444993ac6bbb2b3347473b6fda
Grace Wu <gswu@live.unc.edu>
2017-02-15 18:23:36 -0500

    Initial grace commit

--- index.ios.js
+++ index.ios.js
@@ -1,54 +1,84 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
+  TextInput,
   View
 } from 'react-native';
+import {
+    Button, Footer, FooterTab, Title, Header,Container, Content, Col, Icon, InputGroup, Input, Grid, Row
+} from 'native-base';
 
-export default class AwesomeProject extends Component {
+export default class test extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+        <Container>
+                <Header>
+                    <Button transparent>
+                        <Icon name="menu" />
+                    </Button>
+
+                    <Text>Header</Text >
+
+                    <Button transparent>
+                        <Icon name="heart"/>
+                    </Button>
+                </Header>
+
+                <Content>
+                    <InputGroup borderType="underline" keyboardType = 'numeric'>
+                        <Icon name="heart" style={{color:'#384850'}}/>
+                        <Input placeholder="Heart Rate" />
+                    </InputGroup>
+                </Content>
+
+                <Footer >
+                    <FooterTab>
+                      <Button>
+                          <Icon name="calendar" />
+                      </Button>
+                      <Button>
+                          <Icon name="camera" />
+                      </Button>
+                      <Button active>
+                          <Icon name="heart" />
+                      </Button>
+                      <Button>
+                          <Icon name="bookmarks" />
+                      </Button>
+                    </FooterTab>
+                </Footer>
+            </Container>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
+var styles = StyleSheet.create({
+    toolbar:{
+        backgroundColor:'#81c04d',
+        paddingTop:30,
+        paddingBottom:10,
+        flexDirection:'row'    //Step 1
+    },
+    toolbarButton:{
+        width: 50,            //Step 2
+        color:'#aaff',
+        textAlign:'center'
+    },
+    toolbarTitle:{
+        color:'#fff',
+        textAlign:'center',
+        fontWeight:'bold',
+        flex:1                //Step 3
+    }, 
+    content: {
+        backgroundColor:'#82d3ef',
+        paddingTop:30,
+        paddingBottom:10,
+        flexDirection:'column' 
+    }
 });
 
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
-
+AppRegistry.registerComponent('test', () => test);
