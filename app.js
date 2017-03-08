af7702c26cedb4069e1923c3905252ae357a5b81
Tim Wu <Tim Wu>
2017-03-08 18:38:20 -0500

    Listen for server after db connect

--- app.js
+++ app.js
@@ -6,30 +6,37 @@ var mongoose = require('mongoose');
 
 
 app.use(express.static(__dirname + '/public'));
-app.listen(port, function () {
-	console.log("Server running on port " + port + ".");
-});
+
 
 // Needed for OpenShift. Remove if not using OpenShift
 // var IP_ADDRESS = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var IP_ADDRESS = '127.0.0.1';
 var PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-app.listen(PORT, IP_ADDRESS,() => {
-    console.log(`Express server listening on port ${PORT} in ${app.settings.env} mode`);
+app.get('/', function(req, res){
+	res.send("Home Page");
 });
+
+
 
 var connection_string = "mongodb://localhost:27017/sampledb";
 if(process.env.MONGODB_PASSWORD){
     connection_string = "mongodb://" +
         process.env.MONGODB_USER + ":" +
         process.env.MONGODB_PASSWORD + "@" +
-        "127.0.0.1" + ":" +
+        "mongodb-2-wjzf0" + ":" +
         "27017" + "/" +
         process.env.MONGODB_DATABASE;
 }
 console.log('attempting to connect to MongoDB at ' + connection_string);
-mongoose.connect(connection_string, function(err) { if (err) { throw err; }});
+mongoose.connect(connection_string, function(err) { 
+	if (err) { 
+		throw err; 
+	}
+	app.listen(PORT, IP_ADDRESS,() => {
+    	console.log(`Express server listening on port ${PORT} in ${app.settings.env} mode`);
+	});
+});
 // mongoose.connection.on('error', () => {
 //   console.log('%s MongoDB connection error. Please make sure MongoDB is running.');
 //   process.exit();
