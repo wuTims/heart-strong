a9a18c80771c8217efa6ef4431fa43142067a56c
bpho <bpho@dhcp-v880-10-00326.1Xwireless.unc.edu>
2017-03-08 17:03:44 -0500

    made changes

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ if(process.env.MONGODB_PASSWORD){
         process.env.MONGODB_USER + ":" +
         process.env.MONGODB_PASSWORD + "@" +
         IP_ADDRESS + ":" +
-        process.env.MONGODB_PORT + '/' +
+        "27017" + "/" +
         process.env.MONGODB_DATABASE;
 }
 console.log('attempting to connect to MongoDB at ' + connection_string);
