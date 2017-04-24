59f458d227d8b0d6cc8c0436a4b004a06997c0c1
riaing <panjfang@gmail.com>
2017-04-24 01:01:52 -0400

    curri modify

--- app/Curriculum/Week1/Week1A2.js
+++ app/Curriculum/Week1/Week1A2.js
@@ -20,24 +20,24 @@ export default class JournalInput extends Component {
          <Container>
 
                  <Header>
-                     <Left>
-                        <Button transparent   onPress={() => {this.navigate('Week1Content')}} >
+                       <Left>
+                        <Button transparent  onPress={() => {this.navigate('Week1Content')}} >
                             <Icon name='ios-arrow-round-back'  />
                         </Button>
                     </Left>
                     <Body>
-                        <Title>Question 2 </Title>
+                        <Title>Answer 2 </Title>
                     </Body>
                     <Right />
                 </Header>
 
                 <Content>
-                    <Text>What are your symptoms when you are being to get in trouble with your CHF?
-</Text>
-                    <Input placeholder=" Answer"  style={{color: '#00c497', height: 200}} />
-                    <Button round success onPress= {() => {this.props.navigator.pop()}}  ><Text> previous</Text></Button>
-                    <Button round success onPress= {() => {this.navigate('Week1A2')}}  ><Text> check the answer</Text></Button>
-                    <Button round success onPress= {() => {this.navigate('QuizFinish')}}  ><Text> finish</Text></Button>
+                 <View style={{flex: 1, flexDirection: 'column'}}> 
+                  <View style={{ height: 500}}> 
+                    <Text style={{ textAlign: 'center', fontSize: 20}}>  Shortness of breath, weight gain, swelling of lower extremities.</Text>
+                  </View>
+                    <Button rounded info full onPress= {() => {this.props.navigator.pop()}}  ><Text style={{color: '#FFFFFF'}}> Back to quiz</Text></Button>
+                </View>
 
                 </Content>
 
---
+++ app/Curriculum/Week1/Week1Content.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+
+import { AppRegistry, Text, StyleSheet, View,alertrt, Linking,  Image} from 'react-native';
+import {Container, Content, Input, Icon, Button, Left, Right, Body, Header, Title, ListItem} from 'native-base';
+
+export default class JournalInput extends Component {
+
+  navigate(routeName) {
+      this.props.navigator.push({
+          name: routeName
+      })
+  }
+
+  addToDB() {
+   
+  }
+
+  render(){
+      return (
+         <Container>
+
+                 <Header>
+                     <Left>
+                        <Button transparent   onPress={() => {this.navigate('Curriculum')}} >
+                            <Icon name='ios-arrow-round-back'  />
+                        </Button>
+                    </Left>
+                    <Body>
+                        <Title>Week 1 </Title>
+                    </Body>
+                    <Right />
+                </Header>
+
+                <Content>
+
+              
+                  <View style={{flex:1, flexDirection: 'column'}}>
+                  <Image style={styles.image} source={require('../../../img/week1Content.jpg')}/>
+                    
+                 
+                  
+                    <View style={styles.container}> 
+                        <Text> Some useful Resources </Text> 
+                      	<Text style={{color: 'blue'}}
+             				 	     onPress={() => Linking.openURL('http://www.ksw-gtg.com/aha-heartfailure/#/1/')}>
+          					 	    American Heart  Association 
+        						    </Text>
+
+        						  <Text style={{color: 'blue'}}
+             				 	 onPress={() => Linking.openURL('http://www.heart.org/HEARTORG/Conditions/More/ConsumerHealthCare/Medication-Adherence---Taking-Your-Meds-as-Directed_UCM_453329_Article.jsp#.WL7U1tIrKUk')}>
+          						  American Heart  Association 
+        						  </Text>
+
+        						  <Text style={{color: 'blue'}}
+             				 	 onPress={() => Linking.openURL('http://greatergood.berkeley.edu/article/item/tips_for_keeping_a_gratitude_journal')}>
+          						  Gratitude  Journaling
+        						  </Text>
+                      </View>
+
+                      <View style={{height: 50}}>
+                        <Button rounded info full onPress= {() => {this.navigate('Week1Q1'); this.addToDB()}}  ><Text style={{color: '#FFFFFF' }}> Take the quiz!</Text></Button>
+                      </View>
+                  </View>
+                </Content>
+
+            </Container>
+      );
+    }
+
+}
+
+const styles = StyleSheet.create({
+
+  container: {
+    justifyContent: 'center', 
+    alignItems: 'center',
+    height: 140
+  }, 
+
+  image: {
+    flex: 1,
+    width: 320, 
+    height: 360
+  },
+    
+})
